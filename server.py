7852cb6c7b95b22d027248027b3e61357eb78631
akimov246 <akimov246@yandex.ru>
2023-10-30 20:42:17 +0700

    test

---
+++ server.py
@@ -0,0 +1,11 @@
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("127.0.0.1", 8080))
+
+server.listen()
+
+while True:
+    user, address = server.accept()
+    print("connect")
+    user.send("connect".encode("utf-8"))
